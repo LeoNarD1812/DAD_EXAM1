bdb82429c452f89c1faadabb38265b872758b785
LeoNarD1812 <jesus.huaman181219@gmail.com>
2024-09-15 20:00:42 -0500

    arreglos

--- ms-gateway-server/src/main/java/com/example/msgatewayserver/MsGatewayServerApplication.java
+++ ms-gateway-server/src/main/java/com/example/msgatewayserver/MsGatewayServerApplication.java
@@ -16,9 +16,9 @@ public class MsGatewayServerApplication {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI().info(new Info()
-                .title("OPEN API MICROSERVICIO EVENTO")
+                .title("OPEN API MICROSERVICIO ")
                 .version("0.0.1")
-                .description("servicios web evento")
+                .description("servicios web")
                 .termsOfService("http:// swagger.io/terms")
                 .license(new License().name("Apache 2.0").url("http://springdoc.org"))
         );
