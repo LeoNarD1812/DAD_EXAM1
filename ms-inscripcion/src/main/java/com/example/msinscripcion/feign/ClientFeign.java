7fb759ce289053e43acdc74eee2bc873f7e5bf38
LeoNarD1812 <jesus.huaman181219@gmail.com>
2024-10-20 20:50:33 -0500

    patron circuitbraker

--- ms-inscripcion/src/main/java/com/example/msinscripcion/feign/ClientFeign.java
+++ ms-inscripcion/src/main/java/com/example/msinscripcion/feign/ClientFeign.java
@@ -1,6 +1,7 @@
 package com.example.msinscripcion.feign;
 
 import com.example.msinscripcion.dto.ClientDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,5 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "ms-cliente-service", path = "/cliente")
 public interface ClientFeign {
     @GetMapping("/{id}")
+    @CircuitBreaker(name = "clientListByIdCB", fallbackMethod = "clientListById")
     public ResponseEntity<ClientDto> getById(@PathVariable Integer id);
+    default ResponseEntity<ClientDto> clientListById(@PathVariable Integer id) {
+        return ResponseEntity.ok(new ClientDto());
+    }
 }
